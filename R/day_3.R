267408da155c2c7497ec87ba88962c095972a34c
Joseph Osborne <jd.osborne5@icloud.com>
2021-12-05 23:47:28 +0000

    Including the second part of day 3

--- R/day_3.R
+++ R/day_3.R
@@ -16,9 +16,8 @@ pure_convert_binary_to_int  <- function(.bin_value){
 
 pure_generate_gamma <- function(.input_list){
 	.input_list %>%
-	map(pure_convert_diagnostic_code) %>%
 	reduce(`+`) %>%
-	{.>=(length(input_3)/2)} %>%
+	{.>=(length(.input_list)/2)} %>%
 	as.integer()
 }
 
@@ -31,12 +30,71 @@ pure_generate_epsilon <- function(.input_list){
 
 pure_calculate_power_consumption <- function(.input_list){
 	gamma_val <- .input_list %>%
+		map(pure_convert_diagnostic_code) %>%
 		pure_generate_gamma() %>%
 		pure_convert_binary_to_int()
 
 	epsilon_val <- .input_list %>%
+		map(pure_convert_diagnostic_code) %>%
 		pure_generate_epsilon() %>%
 		pure_convert_binary_to_int()
 
 	gamma_val * epsilon_val
+}
+
+pure_narrow_codes_list <- function(.parsed_list, .gamma_val, .index){
+	list_flags <- .parsed_list %>%
+		map(function(x) x[.index] == .gamma_val[.index]) %>%
+		unlist()
+
+	.parsed_list[list_flags]
+
+
+}
+
+
+pure_calculate_oxygen_generator_rating <- function(.input_list){
+	
+	holding_list  <- .input_list 
+
+	for(i in 1:(length(.input_list[[1]])+1)){
+		gamma_val  <- pure_generate_gamma(holding_list)
+		holding_list <- pure_narrow_codes_list(holding_list, gamma_val, i)		
+		if(length(holding_list) == 1){
+			break
+		}
+	}
+	holding_list[[1]]
+
+}
+
+pure_calculate_co2_scrubber_rating <- function(.input_list){
+	
+	holding_list  <- .input_list
+
+	for(i in 1:(length(.input_list[[1]])+1)){
+		gamma_val  <- pure_generate_epsilon(holding_list)
+		holding_list <- pure_narrow_codes_list(holding_list, gamma_val, i)		
+		if(length(holding_list) == 1){
+			break
+		}
+	}
+	holding_list[[1]]
+
+}
+
+pure_calculate_life_support_rating <- function(.input_list){
+	parsed_input <- .input_list %>%
+		map(pure_convert_diagnostic_code)
+
+	co2_rating <- parsed_input %>%
+		pure_calculate_co2_scrubber_rating() %>%
+		pure_convert_binary_to_int()
+
+	oxygen_rating <- parsed_input %>%
+		pure_calculate_oxygen_generator_rating() %>%
+		pure_convert_binary_to_int()
+
+	co2_rating * oxygen_rating
+
 }
