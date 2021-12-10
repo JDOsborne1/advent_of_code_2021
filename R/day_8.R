c94c7ecb5106ae0ec1bf532937932125cf9f6236
Joseph Osborne <jd.osborne5@icloud.com>
2021-12-10 18:15:23 +0000

    making some good progress in narrowing down the values

--- R/day_8.R
+++ R/day_8.R
@@ -22,15 +22,7 @@ parse_seven_segment_display <- function(.input_lines) {
                 )
 }
 
-# generate_reader_function <- function(
-#         .parsed_signal
-# ){
-#
-#                 .parsed_signal[str_length(.parsed_signal) == 2] |>
-#                         str_split("") |>
-#                         unlist()
-#
-# }
+
 count_all_output_values <- function(
         .parsed_input
 ){
@@ -62,5 +54,147 @@ count_output_values <- function(
 }
 
 
+only <- function(.the_set_of, .makes_up){
+        all(.the_set_of %in% .makes_up) &
+                all(.makes_up %in% .the_set_of)
+}
+
+str_dismantle <- function(.input_string){
+        .input_string |>
+                str_split("") |>
+                unlist()
+}
+
+mapping_digital_display_to_numbers <- function(.display_collection){
+        if (only(
+                .the_set_of = c("RV1", "RV2", "H1", "H3", "LV1", "LV2")
+                , .makes_up = .display_collection
+        )) {
+                0L
+        } else if (only(
+                .the_set_of = c("RV1", "RV2")
+                , .makes_up = .display_collection
+        )) {
+                1L
+        } else if (only(
+                .the_set_of = c("RV1","H1", "H2", "H3", "LV2")
+                , .makes_up = .display_collection
+        )) {
+                2L
+        } else if (only(
+                .the_set_of = c("RV1", "RV2", "H1", "H2", "H3")
+                , .makes_up = .display_collection
+        )) {
+                3L
+        } else if (only(
+                .the_set_of = c("RV1", "RV2", "H2", "LV1")
+                , .makes_up = .display_collection
+        )) {
+                4L
+        } else if (only(
+                .the_set_of = c("H3", "RV2", "H1", "H2", "LV1")
+                , .makes_up = .display_collection
+        )) {
+                5L
+        } else if (only(
+                .the_set_of = c("H2", "RV2", "H1", "H3", "LV1", "LV2")
+                , .makes_up = .display_collection
+        )) {
+                6L
+        } else if (only(
+                .the_set_of = c("RV1", "RV2", "H1")
+                , .makes_up = .display_collection
+        )) {
+                7L
+        } else if (only(
+                .the_set_of = c("RV1", "RV2", "H1", "H2", "H3", "LV1", "LV2")
+                , .makes_up = .display_collection
+        )) {
+                8L
+        } else if (only(
+                .the_set_of = c("RV1", "RV2", "H1", "H2", "H3", "LV1")
+                , .makes_up = .display_collection
+        )) {
+                9L
+        } else {
+                NA_integer_
+        }
+
+}
+
+drop_element <- function(.input_vector, .item_to_drop){
+        modded_vector <- .input_vector
+        for (i in 1:length(.item_to_drop)) {
+                modded_vector <- modded_vector[modded_vector != .item_to_drop[i]]
+        }
+        modded_vector
+}
+
+find_letter_mapping <- function(.parsed_signal){
+        mapping_list <- list(
+        a = c("RV1", "RV2", "H1", "H2", "H3", "LV1", "LV2")
+        , b = c("RV1", "RV2", "H1", "H2", "H3", "LV1", "LV2")
+        , c = c("RV1", "RV2", "H1", "H2", "H3", "LV1", "LV2")
+        , d = c("RV1", "RV2", "H1", "H2", "H3", "LV1", "LV2")
+        , e = c("RV1", "RV2", "H1", "H2", "H3", "LV1", "LV2")
+        , f = c("RV1", "RV2", "H1", "H2", "H3", "LV1", "LV2")
+        , g = c("RV1", "RV2", "H1", "H2", "H3", "LV1", "LV2")
+        )
+
+        ## First Phase reduction
+
+        for (i in .parsed_signal) {
+                if (str_length(i) == 2) {
+                        possible_ones <- str_split(i, "") |>
+                                unlist()
+                        mapping_list[possible_ones] <- mapping_list[possible_ones] |>
+                                map(drop_element,  c("H1", "H2", "H3", "LV1", "LV2"))
+                }
+                if (str_length(i) == 3) {
+                        possible_ones <- str_split(i, "") |>
+                                unlist()
+                        mapping_list[possible_ones] <- mapping_list[possible_ones] |>
+                                map(drop_element,  c("H2", "H3", "LV1", "LV2"))
+                }
+                if (str_length(i) == 4) {
+                        possible_ones <- str_split(i, "") |>
+                                unlist()
+                        mapping_list[possible_ones] <- mapping_list[possible_ones] |>
+                                map(drop_element,  c("H1", "H3", "LV2"))
+                }
+        }
+
+        ## Second Phase reduction
+
+        sorted_signal <- .parsed_signal |>
+                str_length() |>
+                `names<-`(.parsed_signal) |>
+                sort() |>
+                names()
 
 
+        mapping_grid <- mapping_list |>
+                expand.grid()
+
+        for (i in sorted_signal) {
+                valid_combos <- mapping_grid[str_dismantle(i)] |>
+                        unique() |>
+                        mutate(rownum = row_number()) |>
+                        pivot_longer(-rownum) |>
+                        mutate(across(value, as.character)) |>
+                        group_by(rownum) |>
+                        filter(n() == n_distinct(value)) |>
+                        filter(!is.na(mapping_digital_display_to_numbers(value))) |> #summarise(return_number =mapping_digital_display_to_numbers(value))
+                        ungroup() |>
+                        pivot_wider() |>
+                        select(-rownum) |>
+                        as.list() |>
+                        map(unique)
+                mapping_list[names(valid_combos)] <- valid_combos
+
+                # locked_down_values <- mapping_list[map_int(mapping_list, length) == 1]
+
+        }
+
+        mapping_list
+}
